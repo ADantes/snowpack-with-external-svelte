505008c4fdf36ce09001bcd04bcb7b9264a4ac8b
Brian Keyes <b.keyes@blueglassmoon.com>
2020-12-23 12:50:22 -0600

    Snowpack with svelte example

---
+++ app/snowpack.config.js
@@ -0,0 +1,10 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+  ]
+};
---
+++ app/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Button from '@app/form/src/button.svelte';
+</script>
+
+<Button />
